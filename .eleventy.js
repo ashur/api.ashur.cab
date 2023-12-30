66706bb6e533be9503efef54e2ffb40b00a06acd
Ashur Cabrera <hello@ashur.cab>
2023-12-29 18:08:53 -0800

    Remove global robots object

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,9 @@
-const robots = require( "./src/robots.txt.js" );
-
 /* eslint-disable-next-line valid-jsdoc */
 /**
  * @param {import("@11ty/eleventy/src/UserConfig")} eleventyConfig
  */
 module.exports = ( eleventyConfig ) =>
 {
-	eleventyConfig.addGlobalData( "robots", robots );
 	/* Data Extensions */
 	const { dataExtensions } = require( "./src/_eleventy/dataExtensions" );
 	Object.entries( dataExtensions ).forEach( ( [key, value] ) =>
