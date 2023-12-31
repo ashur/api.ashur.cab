3e3666f34ff52617e7b33fbc81a55558b21c8a45
Ashur Cabrera <hello@ashur.cab>
2023-12-30 20:33:50 -0800

    Generate payload in eleventyComputed data
    Using JavaScript directly makes it easier to shape payloads

--- src/endpoints/endpoints.11tydata.js
+++ src/endpoints/endpoints.11tydata.js
@@ -1,15 +1,46 @@
 module.exports = {
 	date: "git Last Modified",
-	layout: "layouts/endpoint.njk",
-	published: new Date(),
+	layout: "layouts/endpoint.json.njk",
 
 	eleventyComputed: {
 		permalink: ( data ) =>
 		{
 			const { page } = data;
 
-			const pathname = page.filePathStem.replace( "/endpoints", "" );
-			return `/${pathname}.json`;
+			const pathname = page.filePathStem
+				.replace( "/endpoints", "" )
+				.replace( /\/index$/, "" );
+
+			return `${pathname}.json`;
+		},
+
+		response: ( data ) =>
+		{
+			const { page, permalink, response, version } = data;
+
+			if ( !permalink )
+			{
+				return;
+			}
+
+			let payload = {};
+			if( Array.isArray( response ) )
+			{
+				const endpointSlug = permalink
+					.split( "/" )[1]
+					.split( "." )[0];
+
+				payload[endpointSlug] = response;
+			}
+			else
+			{
+				payload = { ...response };
+			}
+
+			payload.updated = page.date;
+			payload.version = version;
+
+			return payload;
 		},
 	},
 };
